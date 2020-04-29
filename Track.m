aa5b7b6963ce70caf7a906505add8ab6ff903b40
Aditya Ravichander <aditya.ravichander@gmail.com>
2020-04-30 02:59:25 +0530

    rectified reqd traj and modified Track

--- Track.m
+++ Track.m
@@ -1,6 +1,7 @@
-%% Trajectory planning and Tracking control of underactuated AUV
+% underactuated AUV -- Trajectory planning and Tracking control
 
-%% Initialise variables
+% Initialise variables
+
     %constants
     m11 = 215;
     m22 = 265;
@@ -9,26 +10,24 @@
     d22 = 100;
     d33 = 50;    
     
-    %initial required values
-    xreq = 10;  
-    yreq = 10;      
-    x_dotreq = 0.1;
-    y_dotreq = 0;
-    Reqd = zeros(8,30);               
-    Reqd(1:8,1) = [ 10,10,0.1,0,0,0,0,0]; % [ xreq, yreq, x_dotreq, y_dotreq, psireq, ureq, vreq, rreq ]    
+    %initialise required values
+    Reqd = zeros(8,30);               % [ xreq, yreq, x_dotreq, y_dotreq, psireq, ureq, vreq, rreq ]
+    Reqd(:,1) = [ 0,10,5,0,0,0,0,0];     
     
-    %initial instant vehicle values    
-    Inst = zeros(6,30);
-    Inst(1:6,1) = [ 15,15,0,0,0,0 ];  	  % [ x, y, psi, u, v, r ]
-    m = zeros(1,30);
-    m(1,1) = 1;
+    %initialise and print instantaneous values
+    Inst = zeros(6,30);		      % [ x, y, psi, u, v, r ]
+    Inst(:,1) = [ 0.5,10.5,0,0,0,0 ];    
+    disp('instantaneous values');
+    disp(Inst(:,1));
+    Error = zeros(6,12);              % [ xe, ye, psie, ue, ve, re ]
+    n=0;
     
-%% main LOOP    
+% Control loop
 
-for i = 1:10
-    
-    % values for state variables
-    Error = zeros(6,30);                  % [ xe, ye, psie, ue, ve, re ]
+for i = 1:90
+    %i=1;   
+
+    %% Initial values for state variables
     Error(1,i)   = Inst(1,i)-Reqd(1,i);                                          
     Error(2,i)   = Inst(2,i)-Reqd(2,i);                          
     XE = [ Error(1,i); Error(2,i)];
@@ -42,41 +41,42 @@ for i = 1:10
     Reqd(5,i) = atan(Reqd(4,i)/Reqd(3,i)) - atan(Reqd(7,i)/Reqd(6,i));
     Error(3,i) = Inst(3,i)-Reqd(5,i);
     Error(6,i) = Error(3,i);                               
-    Reqd(8,i) = Inst(6,i)-Error(6,i);   
-    
-    %print reqd values
+    Reqd(8,i) = Inst(6,i)-Error(6,i);
+
+    % print required position and error
+    %disp('error');
+    %disp(Error(:,i+1));         
     disp('reqd');
-    disp(Reqd(1:8,i));
-    
+    disp(Reqd(:,i));    
+
     % Control action by LQR
-    time_period1 = [1 1000];
+    time_period1 = [1 15];
     V0 = [ Error(1,i),Error(2,i),Error(3,i),Error(4,i),Error(5,i),Error(6,i),Reqd(6,i),Reqd(7,i),Reqd(8,i)]; 
-    [t1,p] = ode45('Trackfn',time_period1,V0);        
+    [t1,p] = ode45('Trackfn',time_period1,V0);     
 
-    %print error and instantaneous values
-    Error(1:6,i+1) = [ p(end,1),p(end,2),p(end,3),p(end,4),p(end,5),p(end,6)];     
-    Inst(1:6,i+1) = [ Error(1,i+1)+Reqd(1,i),Error(2,i+1)+Reqd(2,i),Error(3,i+1)+Reqd(5,i),Error(4,i+1)+Reqd(6,i),Error(5,i+1)+Reqd(7,i),Error(6,i+1)+Reqd(8,i) ];
-    m(1,i) = i;
-    disp('error');
-    disp(Error(1:6,i+1));
-    disp('instant position');
-    disp(Inst(1:6,i+1));
-    
-    % update required values
-    time_period2 = [1 10];
+    % plot error and update error
+    figure(1)
+    plot(p(:,1),p(:,2),'g');   % error-green     
+    Error(:,i+1) = [ p(end,1),p(end,2),p(end,3),p(end,4),p(end,5),p(end,6)]; 
+
+    % update and print instantaneous values
+    Inst(:,i+1) = [ Error(1,i+1)+Reqd(1,i),Error(2,i+1)+Reqd(2,i),Error(3,i+1)+Reqd(5,i),Error(4,i+1)+Reqd(6,i),Error(5,i+1)+Reqd(7,i),Error(6,i+1)+Reqd(8,i) ];
+    disp('instantaneous values');
+    disp(Inst(:,i+1));    
+
+    %% update required values
+    time_period2 = [n n+0.15];
     V1 = [ Reqd(1,i), Reqd(2,i), Reqd(3,i), Reqd(4,i) ];
-    [t2,q] = ode45('auv_Track_trajectory',time_period2,V1);
+    [t2,q] = ode45('auv_Track_trajectory',time_period2,V1);    
     Reqd(1:4,i+1) = [ q(end,1),q(end,2),q(end,3),q(end,4)];
+    n=n+0.15;    
 
+    % plot instantaneous position and required trajectory together
+    figure(2)
+    plot(Inst(1,1:i+1),Inst(2,1:i+1),'b');  % instant position-blue
+    hold on
+    plot(Reqd(1,1:i+1),Reqd(2,1:i+1),'r');  % required position-red
+    
 end        
-
-%%    plotting results
-
-	%plot instantaneous values
-       figure(1)
-       plot(Inst(1,1:i),Inst(2,1:i));  
-
-	%plot required trajectory
-       figure(2)
-       plot(Reqd(1,1:i),Reqd(2,1:i));
+      
        
--- auv_Track_trajectory.m
+++ auv_Track_trajectory.m
@@ -1,8 +1,16 @@
 %% Function for ODE solver of Requried trajectory
 function dqdt = auv_Track_trajectory(t2,q)
 
+%disp('reqd');
+%r = [q(1),q(2),q(3),q(4)];
+%disp(r);
+
+a=10;
+b=0.5;
+
 dqdt = zeros(4,1); 
-dqdt(1) = 0.1*cos(0.01*t2);     %to find xreq
-dqdt(2) = -0.1*sin(0.01*t2);	%to find yreq
-dqdt(3) = 0;                    %to find x_dotreq
-dqdt(4) = 0;                    %to find y_dotreq
+dqdt(1) =    (a*b*cos(b*t2));   %to find xreq
+dqdt(2) =   -(a*b*sin(b*t2));	%to find yreq
+dqdt(3) = -(a*b*b*sin(b*t2));   %to find x_dotreq
+dqdt(4) = -(a*b*b*cos(b*t2));   %to find y_dotreq
+
