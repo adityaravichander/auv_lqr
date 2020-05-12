c67600d3bb6c91107adfc0ff534ec9735e9c20ac
Aditya Ravichander <aditya.ravichander@gmail.com>
2020-05-13 01:20:41 +0530

    path tracking

--- Trackfn.m
+++ Trackfn.m
@@ -1,5 +1,6 @@
 function dpdt = Trackfn(t,p)
 
+% Constants
 m11 = 215;
 m22 = 265;
 m33 = 80;
@@ -7,28 +8,37 @@ d11 = 70;
 d22 = 100;
 d33 = 50; 
 
-x_e   = p(1);
-y_e   = p(2);
-psi_e = p(3);
-u_e   = p(4);
-v_e   = p(5);
-r_e   = p(6);
-ureq = p(7);
-vreq = p(8);
-rreq = p(9);   
+% AUV parameters
+x   = p(1);
+y   = p(2);
+psi = p(3);
+u   = p(4);
+v   = p(5);
+r   = p(6);
 
-dpdt = zeros(9,1);    
+% Required parameters
+ureq   = p(7);
+vreq   = p(8);
+rreq   = p(9);   
+xreq   = p(10);
+yreq   = p(11);
+psireq = p(12);
+fureq = p(13);
+frreq = p(14);
 
-xe_mod = abs(x_e);
-ye_mod = abs(y_e);
-e=0.01;
+dpdt = zeros(12,1);    
+
+X = [ (x-xreq); (y-yreq); (psi-psireq); (u-ureq); (v-vreq); (r-rreq)]; % Errors - states of LQR
+xe_mod = abs(X(1));
+ye_mod = abs(X(2));
+e=0.1;
+
+
 
 if((xe_mod>=e)||(ye_mod>=e))
-    X     = [ x_e; y_e; psi_e; u_e; v_e; r_e]; 
-
-    A = [0, -rreq,0,                    -1,                      0,                 0;
-        rreq,  0, 0,                      0,                     -1,                 0;  
-        0,     0, 0,                      0,                      0,                -1;
+    A = [0, -rreq,0,                      1,                      0,                 0;
+        rreq,  0, 0,                      0,                      1,                 0;  
+        0,     0, 0,                      0,                      0,                 1;
         0,     0, 0,             (-d11/m11),       ((m22*rreq)/m11),  ((m22*vreq)/m11);               
         0,     0, 0,      ((-m11*rreq)/m22),             (-d22/m22), ((-m11*ureq)/m22);
         0,     0, 0, (((m11-m22)*vreq)/m33), (((m11-m22)*ureq)/m33),       (-d33/m33)];
@@ -39,67 +49,63 @@ if((xe_mod>=e)||(ye_mod>=e))
         1/m11,      0;
             0,      0;
             0,  1/m33];
-    
-    Q = [ 5,0,0,0,0,0;
-        0,5,0,0,0,0;
-        0,0,5,0,0,0;
-        0,0,0,5,0,0;
-        0,0,0,0,5,0;
-        0,0,0,0,0,5];
-    
-    R = [ 1, 0;
-        0, 1];
-    
-    % C = [ 1 1 1 1 1 1 ];
-    % % disp('C');
-    % % disp(C);
-    
-    % poles = eig(A);
-    % % disp('poles');
-    % % disp(poles);
-    
-    % rankc = rank(ctrb(A,B));
-    % % disp('rankC');
-    % % disp(rankc);
-    
-    % ranko = rank(obsv(A,C));
-    % % disp('rankO');
-    % % disp(ranko);
-%     printmatrix = [ x_e;
-%                         y_e;
-%                         psi_e;    
-%                         u_e;
-%                         v_e;
-%                         r_e;];
-%         disp(printmatrix);
-    
-    K = lqr(A,B,Q,R);
-    F = -(K*X);     
-    X_dot = A*X + B*F;      
-    dpdt(1) = X_dot(1); 
-    dpdt(2) = X_dot(2);      
-    dpdt(3) = X_dot(3);
-    dpdt(4) = X_dot(4);
-    dpdt(5) = X_dot(5);  
-    dpdt(6) = X_dot(6);
-    dpdt(7) = 0;
-    dpdt(8) = 0;
-    dpdt(9) = 0; 
 
-else
-    dpdt(1) = 0; 
-    dpdt(2) = 0;      
-    dpdt(3) = 0;
-    dpdt(4) = 0;
-    dpdt(5) = 0;  
-    dpdt(6) = 0;
-    dpdt(7) = 0;
-    dpdt(8) = 0;
-    dpdt(9) = 0;
+    q = 5;
+    Q =[q,0,0,0,0,0;
+        0,q,0,0,0,0;
+        0,0,q,0,0,0;
+        0,0,0,q,0,0;
+        0,0,0,0,q,0;
+        0,0,0,0,0,q];
+    
+    R = [1,0;
+         0,1];
+
+    K = lqr(A,B,Q,R);
+    F = -(K*X);  
+    fureq = p(13)-F(1);
+    frreq = p(14)-F(2);
 end
 
 
+% ODE solver 
+dpdt(4) = ((m22*v*r)/m11) - ((-d11*u)/m11) + (fureq/m11);
+dpdt(5) = -((m11*u*r)/m22) - ((-d22*v)/m22);  
+dpdt(6) = (((m11-m22)*u*v)/m33) - ((-d33*r)/m33) + (frreq/m33);       
+dpdt(1) = (u*cos(psi)) - (v*sin(psi)); 
+dpdt(2) = (u*sin(psi)) + (v*cos(psi));      
+dpdt(3) = (r);
+dpdt(7) = 0;
+dpdt(8) = 0;
+dpdt(9) = 0;
+dpdt(10)= 0;
+dpdt(11)= 0;
+dpdt(12)= 0;
+dpdt(13)= 0;
+dpdt(14)= 0;
 
+%% Controllability and Observability tests  
 
-
+    %  C = [ 1 1 1 1 1 1 ];
+    %  disp('C');
+    %  disp(C);
+    
+    %  poles = eig(A);
+    %  disp('poles');
+    %  disp(poles);
+    
+    %  rankc = rank(ctrb(A,B));
+    %  disp('rankC');
+    %  disp(rankc);
+    
+    %  ranko = rank(obsv(A,C));
+    %  disp('rankO');
+    %  disp(ranko);
+    %  printmatrix = [ x_e;
+    %                  y_e;
+    %                  psi_e;    
+    %                  u_e;
+    %                  v_e;
+    %                  r_e;];
+    %  disp(printmatrix);    
 
--- path-tracking.m
+++ path-tracking.m
@@ -3,99 +3,75 @@ clear all;
 clc;
 clf;
 
-%%Required Path - circle
-C = [0 0];
-cx = C(1);
-cy = C(2);
-R = 10;
-Reqd = zeros(6,30); % [xr,yr,psir,ur,vr,rr]
-
-%% Initialise constants
-    m11 = 215;
-    m22 = 265;
-    m33 = 80;
-    d11 = 70;
-    d22 = 100;
-    d33 = 50;    
-
-% AUV initialisation 
-auv = zeros(6,10);
-auv(:,1) = [11, 5, 0, 0, 0, 0]; % [x,y,psi,u,v,r]
-x   = auv(1,1);
-y   = auv(2,1);
-psi = auv(3,1);
-u   = auv(4,1);
-v   = auv(5,1);
-r   = auv(6,1);
-
+% AUV parameters
+auv = zeros(6, 100);   % [ x, y, psi, u, v, r ]
+auv(:, 1) = [11, 5, 0, 0, 0, 0];
 disp('auv');
-disp(auv(:,1));
-%Error initialisation
-Error = zeros(6,12);  % [ xe, ye, psie, ue, ve, re ]
-per_dist   = sqrt((x*x)+(y*y))-R;  % distance between AUV and path   
-theta = atan((auv(2,1)-cy)/(auv(1,1)-cx));
-Error(1,1) = per_dist*(cos(theta));        %xe
-Error(2,1) = per_dist*(sin(theta));        %ye
-Reqd(1,1)  = auv(1,1)   - Error(1,1);      %xr
-Reqd(2,1)  = auv(2,1)   - Error(2,1);      %yr
+disp(auv(:, 1));
+
+%%Required Path - circle
+cx = 0;
+cy = 0;
+R = 10;
+Reqd = zeros(6, 100);  % [ x_req, y_req, psi_req, u_req, v_req, r_req ]
+Fin = [0,0];
+%Error parameters
+Error = zeros(6, 100); % [ x_error, y_error, psi_error, u_error, v_error, r_error ]
+
 %    i=1;
-% loop for updating 
-for i=1:180
-    % Calculate required values and error
-    disp(i);    
-    theta = atan((auv(2,i)-cy)/(auv(1,i)-cx));
-    Reqd(3,i)  = 1.57       + theta;           %psir
-    Error(3,i) = auv(3,i)   - Reqd(3,i);       %psie
-    XE         = [ Error(1,i); Error(2,i)];
-    R1         = [ cos(auv(3,i)), -sin(auv(3,i));
-                   sin(auv(3,i)), cos(auv(3,i))];
-    UVE        = -(transpose(R1)*XE);
-    Error(4,i) = UVE(1);       		       %ue	           
-    Error(5,i) = UVE(2);		       %ve 
-    Reqd(4,i)  = auv(4,i)   - Error(4,i);      %ur
-    Reqd(5,i)  = auv(5,i)   - Error(5,i);      %vr
 
-    if(i==1)
-	Error(6,i) = Error(3,i);               
+for i = 1:50
+
+    % Calculate Required values
+    disp(i);
+    per_dist = sqrt((auv(1, i)^2) + (auv(2, i)^2)) - R; % perpendicular distance between AUV and path
+    theta = atan((auv(2, i) - cy) / (auv(1, i) - cx));  % angle between point and center of circle
+
+    Error(1, i) = per_dist * (cos(theta));              % x_error
+    Error(2, i) = per_dist * (sin(theta));              % y_error
+    Reqd(1, i) = auv(1, i) - Error(1, i);               % x_req
+    Reqd(2, i) = auv(2, i) - Error(2, i);               % y_req
+    Reqd(3, i) = 1.57 + theta;                          % psi_req
+    Error(3, i) = auv(3, i) - Reqd(3, i);               % psi_error
+    XYE = [Error(1, i); Error(2, i)];                   % array of x_error and y_error
+    R1 = [cos(auv(3, i)), -sin(auv(3, i));              % Rotation matrix
+          sin(auv(3, i)), cos(auv(3, i))];
+    UVE = -(transpose(R1) * XYE);                       % array of u_error and v_error
+    Error(4, i) = UVE(1);                               % u_error 
+    Error(5, i) = UVE(2);                               % v_error
+    Reqd(4, i) = auv(4, i) - Error(4, i);               % u_req
+    Reqd(5, i) = auv(5, i) - Error(5, i);               % v_req
+
+    if (i == 1)
+        Error(6, i) = Error(3, i);      % + > top
     else
-	Error(6,i) = Error(3,i) - Error(3,i-1);%re
-    end 
-    Reqd(6,i)  = auv(6,i)   - Error(6,i);      %rr
+        Error(6, i) = (Error(3, i) - Error(3, i - 1)); % r_error  -> circle
+    end
+    Reqd(6, i) = auv(6, i) - Error(6, i);               % r_req
     
- 
-    %Control action by LQR
-    time_period1 = [0 600];
-    V0 = [ Error(1,i),Error(2,i),Error(3,i),Error(4,i),Error(5,i),Error(6,i),Reqd(4,i),Reqd(5,i),Reqd(6,i)]; 
-    [t1,p] = ode45('Trackfn',time_period1,V0);  
-    %figure(2)
-    %plot(p(:,1),t1,'g');   % error-green 
+    % Display reqd position and position error
+    disp('reqdpos');
+    disp(Reqd(1:2,i));
+    disp('errorpos');
+    disp(Error(1:2,i));
     
-    figure(3)
-    plot((Reqd(1,i)+p(:,1)),(Reqd(2,i)+p(:,2)),'b')
-    hold on 
-    plot(C(1)+R*cos([1:360].*pi/180),C(2)+R*sin([1:360].*pi/180),'r'); hold on;
-
+    % Control action by LQR
+    time_period = [0 1];
+    V0 = [auv(1, i), auv(2, i), auv(3, i), auv(4, i), auv(5, i), auv(6, i), Reqd(4, i), Reqd(5, i), Reqd(6, i), Reqd(1, i), Reqd(2, i), Reqd(3, i), Fin(1), Fin(2)];
+    [t1, p] = ode45('Trackfn', time_period, V0);
     
-    Error(:,i+1) = [ p(end,1),p(end,2),p(end,3),p(end,4),p(end,5),p(end,6)];     
-    auv(:,i+1) = [ Error(1,i+1)+Reqd(1,i),Error(2,i+1)+Reqd(2,i),Error(3,i+1)+Reqd(3,i),Error(4,i+1)+Reqd(4,i),Error(5,i+1)+Reqd(5,i),Error(6,i+1)+Reqd(6,i) ];
+    % Plot auv's path and required path
+    figure(1)
+    plot(p(:, 1), p(:, 2), 'b')                         % plots auv's path
+    hold on
+    plot(cx + R * cos([1:360] .* pi / 180), cy + R * sin([1:360] .* pi / 180), 'r'); % plots required path
+
+    % Update auv parameters
+    auv(:, i + 1) = [p(end, 1), p(end, 2), p(end, 3), p(end, 4), p(end, 5), p(end, 6)];
+
+    % Display auv position
+    disp('auvpos');
+    disp(auv(1:2,i+1));
+end
 
 
-    %disp('error');
-    %disp(p(:,1:2));
-     
-    % Display
-    disp('reqd');
-    disp(Reqd(:,i));
-
-    disp('auv');
-    disp(auv(:,i+1)); 
-    
-    Reqd(1,i+1) = (C(1)+(R*cos(2*i*pi/180)));  % next xr
-    Reqd(2,i+1) = (C(2)+(R*sin(2*i*pi/180)));  % next yr
-    Error(1,i+1) = auv(1,i+1) - Reqd(1,i+1);
-    Error(2,i+1) = auv(2,i+1) - Reqd(2,i+1);
-end       
-figure(1)
-plot(C(1)+R*cos([1:360].*pi/180),C(2)+R*sin([1:360].*pi/180),'r'); hold on;
-hold on
-plot(auv(1,1:i+1),auv(2,1:i+1))
