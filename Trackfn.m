7dd6a4c676deee1f3dc01d5542ff5fc7068c6488
Aditya Ravichander <aditya.ravichander@gmail.com>
2020-06-15 20:39:26 +0530

    changed to 3-states

--- Trackfn.m
+++ Trackfn.m
@@ -1,84 +1,33 @@
 function dpdt = Trackfn(t,p)
     
-    % Constants
-    mass= 185; % Mass kg
-    Iz = 50; % Rotational inertia kg-m^2
-    Xu = -30; % added mass kg
-    Yv = -90; % added mass kg
-    Nr = -30; % added mass kg
-    d11 = 70; % surge linear drag
-    d22 = 100;% sway linear drag
-    d33 = 50; % yaw linear drag
-    
-    % combined inertia and added mass terms
-    m11 = mass - Xu; %kg 
-    m22 = mass - Yv; %kg
-    m33 = Iz- Nr;    %kg-m^2
-    
     % Global variables
-    global Fin
-    global i
-    global Reqd
-    global Error
-    
-    dpdt = zeros(6,1);   
-    
-    A = [       0, -Reqd(6,i),  0,                           1,                           0,                      0;
-        Reqd(6,i),           0, 0,                           0,                           1,                      0;  
-                0,           0, 0,                           0,                           0,                      1;
-                0,           0, 0,                  (-d11/m11),       ((m22*Reqd(6,i))/m11),  ((m22*Reqd(5,i))/m11);               
-                0,           0, 0,      ((-m11*Reqd(6,i))/m22),                  (-d22/m22), ((-m11*Reqd(4,i))/m22);
-                0,           0, 0, (((m11-m22)*Reqd(5,i))/m33), (((m11-m22)*Reqd(4,i))/m33),            (-d33/m33)];
-        
-    B = [   0,      0;
-            0,      0;
-            0,      0;
-        1/m11,      0;
-            0,      0;
-            0,  1/m33];
-    
-    % Tuning matrices    
-    Q = 1*eye(6);
-    R = 1*eye(2);
-    
-    % LQR gain and Control Input
-    K = lqr(A,B,Q,R);    % gain from LQR
-    F = -(K*Error(:,i)); % returns error in Fin 
-        
-    % Updating Fin values
-    Fin(1) = Fin(1)-F(1);
-    Fin(2) = Fin(2)-F(2);
-    
+    global Fin  % Force input
+    global d    % linear drag coefficient
+    global m    % combined inertia and added mass terms
+
     % ODE solver 
-    dpdt(4) = ((m22*p(5)*p(6))/m11) - ((-d11*p(4))/m11) + (Fin(1)/m11);       % to find u
-    dpdt(5) = -((m11*p(4)*p(6))/m22) - ((-d22*p(5))/m22);                     % to find v
-    dpdt(6) = (((m11-m22)*p(4)*p(5))/m33) - ((-d33*p(6))/m33) + (Fin(2)/m33); % to find r
-    dpdt(3) = (p(6));                                                         % to find psi
-    dpdt(1) = (p(4)*cos(p(3))) - (p(5)*sin(p(3)));                            % to find x
-    dpdt(2) = (p(4)*sin(p(3))) + (p(5)*cos(p(3)));                            % to find y
+    dpdt = zeros(6,1);   
+ 
+    dpdt(4) = ((m(2)*p(5)*p(6))/m(1)) + ((-d(1)*p(4))/m(1)) + (Fin(1)/m(1));       % to find u
+
+    dpdt(5) = (-(m(1)*p(4)*p(6))/m(2)) + ((-d(2)*p(5))/m(2));                      % to find v
+
+    dpdt(6) = (((m(1)-m(2))*p(4)*p(5))/m(3)) + ((-d33*p(6))/m(3)) + (Fin(2)/m(3)); % to find r
+
+    dpdt(3) = (p(6));                                                              % to find psi
+
+    dpdt(1) = (p(4)*cos(p(3))) + (p(5)*sin(p(3)));                                 % to find x
+
+    dpdt(2) = (p(4)*sin(p(3))) - (p(5)*cos(p(3)));                                 % to find y
     
+
+
+
+
+
     %% Controllability and Observability tests  
-    
         %  C = [ 1 1 1 1 1 1 ];
-        %  disp('C');
-        %  disp(C);
-        
         %  poles = eig(A);
-        %  disp('poles');
-        %  disp(poles);
-        
-        %  rankc = rank(ctrb(A,B));
-        %  disp('rankC');
-        %  disp(rankc);
-        
+        %  rankc = rank(ctrb(A,B));        
         %  ranko = rank(obsv(A,C));
-        %  disp('rankO');
-        %  disp(ranko);
-        %  printmatrix = [ x_e;
-        %                  y_e;
-        %                  psi_e;    
-        %                  u_e;
-        %                  v_e;
-        %                  r_e;];
-        %  disp(printmatrix);    
         
--- path-tracking.m
+++ path-tracking.m
@@ -1,71 +1,75 @@
 % Trajectory planning and Tracking control of underactuated AUV
 
-% AUV values 
-auv = zeros(6, 100);   % [ x, y, psi, u, v, r ] Auv values in global frame
-auv(:, 1) = [11, 5, 0, 0, 0, 0];
-disp('Auv');
-disp(auv(:, 1));
+% Constants
+global m
+global d
+mass= 185;                    % Mass kg
+Iz = 50;                      % Rotational inertia kg-m^2
+Xu = -30;                     % added mass kg
+Yv = -90;                     % added mass kg
+Nr = -30;                     % added mass kg
+m = [mass-Xu, mass-Yv, Iz-Nr] % combined inertia and added mass terms
+d = [70, 100, 50];            % linear drag [surge, sway, yaw]
 
 % Path values
 cx = 0; % x coordinate of center of circle
 cy = 0; % y coordinate of center of circle
 R = 10; % Radius of circle
-
-% Global values 
-global Fin
-Fin = [0, 0];
-global Reqd 
 Reqd = zeros(6, 200);  % [ x_req, y_req, psi_req, u_req, v_req, r_req ] Required values in global frame
-global Error
-Error = zeros(6, 200); % body frame [ x_error, y_error, psi_error, u_error, v_error, r_error ] 
-global i
-xyGE = zeros(2, 200);  % global frame [ x_error, y_error]
+
+% AUV values 
+auv = zeros(6, 100);   % [ x, y, psi, u, v, r ] Auv values in global frame
+auv(:, 1) = [10.5, 5, 0, 0, 0, 0];
+disp('Auv');
+disp(auv(:, 1)); 
+global Fin             
+Fin = [0, 0];          % Force input
+Error = zeros(6, 200); % Error values [ x_error, y_error, psi_error, u_error, v_error, r_error ] 
 
 %% Path Tracking
 
 for i = 1:200
+    disp(i);
 
     % Calculate perpendicular distance to circle
-    disp(i);
-    per_dist = (sqrt((auv(1,i)^2) + (auv(2,i)^2))) - R;  % shortest distance between AUV and path
+    per_dist = ((sqrt((auv(1,i)^2) + (auv(2,i)^2))) - R);  % shortest distance between AUV and path
     disp('per_dist');
     disp(per_dist);
     
     % Theta - angle between auv and center of circle 
     theta = angwrapfn(atan2((auv(2,i) - cy),(auv(1,i) - cx)));  % psi_required in range ( 0 to 6.28 )        
-    disp('theta')
+    disp('theta');
     disp(theta);
     
     % PSI Error and Reqd values 
-    Reqd(3,i) = angwrapfn(1.57 + theta);   % psi_required - direction of tangent to circle
-    Error(3,i) = auv(3,i) - Reqd(3,i);     % psi_error
+    Reqd(3,i) = angwrapfn(1.57 + theta);    % psi_required - direction of tangent to circle
+    Error(3,i) = auv(3,i) - Reqd(3,i);      % psi_error
     
     % Position Error and Reqd values 
-    xyGE(1:2,i) = [ per_dist * (cos(theta)); per_dist * (sin(theta)) ];  % position error global frame
-    Reqd(1,i) = auv(1,i) - xyGE(1);        % x_required
-    Reqd(2,i) = auv(2,i) - xyGE(2);        % y_required
-    
-    R1 = [ cos(auv(3,i)), -sin(auv(3,i));   % Rotation matrix 1
-           sin(auv(3,i)), cos(auv(3,i))];
-       
-    R2 = [-cos(Reqd(3,i)), +sin(Reqd(3,i)); % Rotation matrix 2
-          -sin(Reqd(3,i)), -cos(Reqd(3,i))];
-       
-    Error(1:2,i) = (R1 * xyGE(1:2,i));     % position error in body frame
+    Error(1:2,i) = [ per_dist*(cos(theta)); per_dist*(sin(theta)) ];  % position error
+    Reqd(1,i) = auv(1,i) - Error(1);        % x_required
+    Reqd(2,i) = auv(2,i) - Error(2);        % y_required
 
     % Velocity Error and Reqd Values 
-    ts = 1;                                % sample time
+    ts = 0.01;                              % sample time
     if (i==1)
-        xydot_GE = xyGE(1:2,i)/ts;         % velocity error in global frame
-        Error(6,i) = Error(3,i)/ts;        % r_error
+        xydot_GE = Error(1:2,i)/ts;         % velocity error in global frame
+        Error(6,i) = Error(3,i)/ts;         % r_error
     else
-        xydot_GE = [((xyGE(1,i) - xyGE(1,i-1))/ts);  ((xyGE(2,i) - xyGE(2,i-1))/ts)]; % velocity error in global frame
-        Error(6,i) = ((Error(3,i) - Error(3,i-1))/ts);   % r_error
+        xydot_GE = [((Error(1,i) - Error(1,i-1))/ts);  ((Error(2,i) - Error(2,i-1))/ts)]; % velocity error in global frame
+        Error(6,i) = ((Error(3,i) - Error(3,i-1))/ts); % r_error
     end 	
-    
-    Reqd(4:5,i) = (R2)\(xydot_GE - (R1*auv(3:4,i)));
-    Error(4:5,i) = auv(4:5,i) - Reqd(4:5,i); % velocity error in body frame   
-    Reqd(6,i) = auv(6,i) - Error(6,i);       % r_req
+
+    % Rotation matrix 1
+    R1 = [ cos(Reqd(3,i)),  sin(Reqd(3,i)); 
+           sin(Reqd(3,i)), -cos(Reqd(3,i))];
+    % Rotation matrix 2   
+    R2 = [-cos(Reqd(3,i)) - (sin(Reqd(3,i))*Error(3,i)) , -sin(Reqd(3,i)) + (cos(Reqd(3,i))*Error(3,i)) ; 
+          -sin(Reqd(3,i)) + (cos(Reqd(3,i))*Error(3,i)) ,  cos(Reqd(3,i)) + (sin(Reqd(3,i))*Error(3,i))];
+       
+    Reqd(4:5,i) = (R2)\(xydot_GE - (R1*auv(3:4,i))); % ureq and vreq 
+    Error(4:5,i) = auv(4:5,i) - Reqd(4:5,i);         % u_error and v_error   
+    Reqd(6,i) = auv(6,i) - Error(6,i);               % r_req
     
     % Display
     disp('reqd');
@@ -73,20 +77,36 @@ for i = 1:200
     %disp('errorpos');
     %disp(Error(1:6,i));
     
-    % Control action by LQR
-    time_period = [0 ts];
-    V0 = auv(:,i);
-    [t1, p] = ode45('Trackfn', time_period, V0);
+    % State model
+    A = [                    (-d11/m11),       ((m22*Reqd(6,i))/m11),  ((m22*Reqd(5,i))/m11);               
+                 ((-m11*Reqd(6,i))/m22),                  (-d22/m22), ((-m11*Reqd(4,i))/m22);
+            (((m11-m22)*Reqd(5,i))/m33), (((m11-m22)*Reqd(4,i))/m33),            (-d33/m33)];
+        
+    B = [ 1/m11,      0;
+              0,      0;
+              0,  1/m33];
     
+    % Tuning matrices    
+    Q = 1*eye(3);
+    Rt = 1*eye(2);
+    
+    % Control action by LQR
+    K = lqr(A,B,Q,Rt);     % gain from LQR
+    F = -(K*Error(4:6,i)); % returns error in Fin     
+    Fin = Fin-F;           % Updating Fin values
+    
+    % ODE solver to update auv values
+    time_period = [0 ts];                           % time period for ODE45
+    V0 = auv(:,i);                                  % input to ODE45
+    [t1, p] = ode45('Trackfn', time_period, V0);    % ODE45 
+    auv(:,i+1) = p(end,:);                          % Update auv parameters            
+    auv(3,i+1) = angwrapfn(auv(3,i+1));             % psi in ( 0 to 6.28 )
+
     % Plots
     figure(1)
     plot(p(:,1), p(:,2), 'b')   % plots auv's path
     hold on
-    plot(cx + R * cos([1:360] .* pi / 180), cy + R * sin([1:360] .* pi / 180), 'r'); % plots required path
-
-    % Update auv parameters
-    auv(:,i+1) = p(end,:);             
-    auv(3,i+1) = angwrapfn(auv(3,i+1)); % psi in ( 0 to 6.28 )
+    plot(cx + (R * cos([1:360] .* pi / 180)), cy + (R * sin([1:360] .* pi / 180)), 'r'); % plots required path
 
     % Display 
     disp('Fin');
